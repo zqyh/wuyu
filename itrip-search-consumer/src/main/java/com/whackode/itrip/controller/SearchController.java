edfd73d882c85c9faf47ef7a755be44a6dbf48e7
Arthur <whackode@163.com>
2020-03-15 20:31:18 +0800

    创建爱旅行itrip-search-consumer项目

---
+++ itrip-search-consumer/src/main/java/com/whackode/itrip/controller/SearchController.java
@@ -0,0 +1,41 @@
+package com.whackode.itrip.controller;
+
+import com.whackode.itrip.base.controller.BaseController;
+import com.whackode.itrip.base.pojo.vo.ResponseDto;
+import com.whackode.itrip.pojo.entity.Hotel;
+import com.whackode.itrip.pojo.vo.HotelVO;
+import com.whackode.itrip.pojo.vo.SearchHotCityVO;
+import com.whackode.itrip.transport.HotelTransport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * <b>爱旅行-搜索模块控制器</b>
+ * @author Arthur
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@RestController("searchController")
+@RequestMapping("/search/api")
+public class SearchController extends BaseController {
+	@Autowired
+	private HotelTransport hotelTransport;
+
+	/**
+	 * <b>根据热门城市查询酒店</b>
+	 * @param queryVO
+	 * @return
+	 * @throws Exception
+	 */
+	@PostMapping(value = "/hotellist/searchItripHotelListByHotCity")
+	public ResponseDto<Object> searchItripHotelListByHotCity(@RequestBody SearchHotCityVO queryVO)
+			throws Exception {
+		List<HotelVO> hotelList = hotelTransport.searchItripHotelListByHotCity(queryVO);
+		return ResponseDto.success(hotelList);
+	}
+}
